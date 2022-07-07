28af6f1caac3cd673a71b48ab1278df034b11ca6
ohitsmekatie <katie.sipos@gmail.com>
2022-07-07 19:44:50 +0000

    add v1 of sessions sql

---
+++ models/intermediate/sessions.sql
@@ -0,0 +1,33 @@
+with last_events as (
+  select 
+    *,
+    lag(timestamp, 1) 
+    over (partition by visitor_id order by timestamp) as prior_page_visit,
+  from {{ ref('stitched_pageviews') }}
+),
+
+time_diff as (
+  select 
+    *,
+    date_diff(timestamp, prior_page_visit, second) as diff_in_seconds
+  from last_events 
+),
+
+sessions as (
+  select 
+    *,
+    case 
+      when diff_in_seconds > (60 * 30) or prior_page_visit is null then 1 else 0 end as is_new_session
+  from time_diff
+)
+
+select 
+  customer_id,
+  visitor_id,
+  coalesce(customer_id,"anon-user") || '-' || sum(is_new_session) over (partition by customer_id order by timestamp) as session_id,
+  timestamp,
+  prior_page_visit,
+  page,
+  diff_in_seconds,
+  is_new_session
+from sessions
