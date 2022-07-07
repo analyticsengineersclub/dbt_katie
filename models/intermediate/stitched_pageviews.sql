32199d656d6c80803ac23ffc066f873441f74ee0
ohitsmekatie <katie.sipos@gmail.com>
2022-07-07 19:17:43 +0000

    add intermediate folder & stitched sql

---
+++ models/intermediate/stitched_pageviews.sql
@@ -0,0 +1,35 @@
+with total_pageviews as (
+  select * 
+  from {{ ref('stg_pageviews')}}
+),
+
+add_visit_counts as (
+  select 
+    *, 
+    count(visitor_id) over (partition by customer_id order by timestamp) as num_visit
+  from total_pageviews
+  where customer_id is not null 
+),
+
+stitched_id as (
+  select 
+  customer_id,
+  visitor_id as first_visit_id
+  from add_visit_counts 
+  where 
+    num_visit = 1 
+)
+
+select 
+  total_pageviews.pageview_id,
+  total_pageviews.customer_id,
+  case 
+    when total_pageviews.customer_id is null then total_pageviews.visitor_id
+    else stitched_id.first_visit_id end
+    as visitor_id,
+  total_pageviews.device_type,
+  total_pageviews.timestamp,
+  total_pageviews.page
+from total_pageviews
+left join stitched_id 
+    on total_pageviews.customer_id = stitched_id.customer_id 
