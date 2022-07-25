7404e38f122ae5c9af4c3ed3305f7fa410712a08
ohitsmekatie <katie.sipos@gmail.com>
2022-07-25 13:45:14 +0000

    add visitors sql from solution

---
+++ models/staging/web/visitors.sql
@@ -0,0 +1,24 @@
+-- from claire's solution which is much more clean & accurate over my original
+
+with visitors as (
+  select visitor_id,
+
+  first_value(customer_id ignore nulls) over (
+    partition by visitor_id
+    order by timestamp
+    range between unbounded preceding and unbounded following
+    ) as first_customer_id,
+
+    min(timestamp) over (partition by visitor_id) as first_seen_at,
+
+    max(timestamp) over (partition by visitor_id) as last_seen_at
+
+  from {{ source('web_tracking', 'pageviews')}}
+)
+
+select 
+  *,
+  first_value(visitor_id) over (
+    partition by coalesce(first_customer_id, visitor_id) 
+    order by first_seen_at) as first_visitor_id 
+from visitors 
