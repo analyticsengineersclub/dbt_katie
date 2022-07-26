ac551c2aca91c6c2fdb9b88bdfc5084342dfc2d9
ohitsmekatie <katie.sipos@gmail.com>
2022-07-26 13:49:25 +0000

    dbt utils date spine table

---
+++ models/all_weeks.sql
@@ -0,0 +1,13 @@
+with generated_weeks as (
+    {{ dbt_utils.date_spine(
+    datepart="week",
+    start_date="cast('2021-01-03' as date)",
+    end_date="cast('2021-10-01' as date)"
+   )
+}}
+)
+
+select 
+    cast(date_week as timestamp) as date_week
+from generated_weeks
+
